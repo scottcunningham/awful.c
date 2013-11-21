363515111ddabccdd56ffe35a508051e4e644882
Scott Cunningham <cunninsc@tcd.ie>
2013-11-21 22:32:00 +0000

    wham bam

---
+++ awful.c
@@ -0,0 +1,13 @@
+%: include <stdio.h>
+%: define __ printf("%d",j)
+
+int main()<%
+    int _
+        <:1:>,j=-??- 0x001fdd;
+    _ ??(
+            3 ??) 
+    =1;
+    _??( 14 ??)= 0;
+    while (_
+            [j++]
+    ) __ ;%>
